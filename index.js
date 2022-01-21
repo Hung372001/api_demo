d196400f0f037b8e52dd41ec37ec69c9b675c2ab
Your Name <ocoten321@gmail.com>
2022-01-21 11:31:16 +0700

    api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 44806;
+const port = process.env.PORT || 5000;
 const employee = require("./src/routes/employee.route.js");
 
 app.get("/", (req, res) => {
