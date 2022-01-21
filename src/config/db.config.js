a9b494a21013b63a973cfb91fc3a2f909c619297
Your Name <ocoten321@gmail.com>
2022-01-21 12:51:14 +0700

    api

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql");
 const dbConn = mysql.createConnection({
-  host: "127.0.0.1",
+  host: "http://127.0.0.1/phpmyadmin/",
   user: "root",
   password: "",
   database: "node_mysql_crud_db",
-  port: "3306",
+  port: "3036",
 });
 dbConn.connect(function (err) {
   if (err) throw err;
