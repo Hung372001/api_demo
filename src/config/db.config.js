bcba58388ddae4ffc77fe1abdf6bdd714a288429
Your Name <ocoten321@gmail.com>
2022-01-23 01:06:59 +0700

    api

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -3,7 +3,7 @@ const dbConn = mysql.createConnection({
   host: "mysql-testapi01.alwaysdata.net",
   user: "testapi01_abc",
   password: "apidemo",
-  database: "testapi01_node_js	 ",
+  database: "testapi01_node_js",
 });
 dbConn.connect(function (err) {
   if (err) throw err;
